4d10daafceb624a746be1445d139a2a53e229dff
sveynsson <rodemannsven@gmail.com>
2025-02-13 08:25:05 +0100

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  base: '/TRAtoKML_Web/', // Genaue Schreibweise des Repo-Namens!
-  plugins: [react()],
-});
