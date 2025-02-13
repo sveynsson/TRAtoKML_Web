0173298a4696a73a75d260d4176811ac750d299b
sveynsson <148841358+sveynsson@users.noreply.github.com>
2025-02-13 08:22:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/TRAtoKML_Web/', // Korrekte Schreibweise aus GitHub
+  base: '/TRAtoKML_Web/', // Genaue Schreibweise des Repo-Namens!
   plugins: [react()],
 });
